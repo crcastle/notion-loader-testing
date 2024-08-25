bfaefc12acb8385f69751eecd69424958ef923f8
Chris Castle <github@crc.io>
2024-08-25 15:30:18 -0700

    Initial commit

---
+++ src/content/config.ts
@@ -0,0 +1,15 @@
+import { defineCollection } from "astro:content";
+import { notionLoader } from "notion-astro-loader";
+
+const database = defineCollection({
+    loader: notionLoader({
+      auth: import.meta.env.NOTION_TOKEN,
+      database_id: import.meta.env.NOTION_DATABASE_ID,
+      filter: {
+        property: "Hidden",
+        checkbox: { equals: false },
+      },
+    }),
+  });
+  
+  export const collections = { database };
