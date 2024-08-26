d4cea84230c6e8ac2a19c2cec4ce1abd5f9b9994
Chris Castle <github@crc.io>
2024-08-26 14:27:13 -0700

    Switch to netlify server rendering

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,11 @@
 import { defineConfig } from 'astro/config';
+import netlify from "@astrojs/netlify";
 
+// https://astro.build/config
 export default defineConfig({
   experimental: {
     contentLayer: true
-  }
-})
+  },
+  output: "server",
+  adapter: netlify()
+});
